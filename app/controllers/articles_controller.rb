75fe9e82b1097167c7a87b608e02048be2224fc2
SatnamSingh <satnam.ss940@gmail.com>
2020-06-25 14:51:58 +0530

    added login form +  restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def index
-    @article = Article.all
+    @article = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def show
@@ -11,7 +13,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created Successfully"
       redirect_to articles_path
@@ -37,8 +39,18 @@ class ArticlesController < ApplicationController
   end
    
   def destroy
-    @article.destroy
-    redirect_to articles_path
+    if @article.destroy
+      puts "------------------------------"
+      puts "index destroy"
+      puts "------------------------------"
+      puts "------------------------------"
+      redirect_to articles_path
+    else
+      puts "*************************************"
+      puts "*************************************"
+      puts "*************************************"
+      puts "*************************************"
+    end
   end
 
   private
@@ -49,5 +61,12 @@ class ArticlesController < ApplicationController
   
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+    flash[:alert] = "You can only edit or delete your own article"
+    redirect_to @article
+  end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 	get 'signup', to: 'users#new'
 	post 'users', to: 'users#create'
 	resources :users, except: [:new]
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	get 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
